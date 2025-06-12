31adab1d6e153459ef7fd9caf242d45f76a6889a
GM <georgemkrs@gmail.com>
2025-06-12 21:06:08 +0300

    Update ConfigurationStructure.ps1

--- ActiveDirectory/ConfigurationStructure.ps1
+++ ActiveDirectory/ConfigurationStructure.ps1
@@ -4,38 +4,38 @@ $givenName = "Arthur"
 $surName = "Morgan"
 $fullUserName = "arthurmorgan"
 $passPhrase = "Password!@#1"
-$topOrganizationUnit = "OU=Valentine,DC=domain,DC=com"
-$tierZeroOU = "OU=Tier0-Control,OU=Valentine,DC=domain,DC=com"
-$tierOneOU = "OU=Tier1-Management,OU=Valentine,DC=domain,DC=com"
-$tierTwoOU = "OU=Tier2-Users,OU=Valentine,DC=domain,DC=com"
+$topOU = "OU=Valentine,DC=domain,DC=com"
+$tier0OU = "OU=Tier0-Control,OU=Valentine,DC=domain,DC=com"
+$tier1OU = "OU=Tier1-Management,OU=Valentine,DC=domain,DC=com"
+$tier2OU = "OU=Tier2-Users,OU=Valentine,DC=domain,DC=com"
 
 # Create Top level container and Tiers
 New-ADOrganizationalUnit -Name "Hierarchy-Top"
-New-ADOrganizationalUnit -Name "Tier0-Control" -Path $topOrganizationUnit
-New-ADOrganizationalUnit -Name "Tier1-Management" -Path $topOrganizationUnit
-New-ADOrganizationalUnit -Name "Tier2-Users" -Path $topOrganizationUnit
+New-ADOrganizationalUnit -Name "Tier0-Control" -Path $topOU
+New-ADOrganizationalUnit -Name "Tier1-Management" -Path $topOU
+New-ADOrganizationalUnit -Name "Tier2-Users" -Path $topOU
 
 # Create Tier0-Control Structure
-New-ADOrganizationalUnit -Name "Domain-Controllers" -Path $tierZeroOU
-New-ADOrganizationalUnit -Name "Tier0-Admin-Accounts" -Path $tierZeroOU
-New-ADOrganizationalUnit -Name "Tier0-Infrastructure-Servers" -Path $tierZeroOU
-New-ADOrganizationalUnit -Name "Tier0-Security-Groups" -Path $tierZeroOU
-New-ADOrganizationalUnit -Name "Tier0-Service-Accounts" -Path $tierZeroOU
-
-Set-ADComputer -Identity "CN=DC01,OU=Domain Controllers,OU=Tier0-Control,OU=Valentine,DC=domain,DC=com" -Description "Primary Domain Controller"
+New-ADOrganizationalUnit -Name "Domain-Controllers" -Path $tier0OU
+New-ADOrganizationalUnit -Name "Tier0-Admin-Accounts" -Path $tier0OU
+New-ADOrganizationalUnit -Name "Tier0-Infrastructure-Servers" -Path $tier0OU
+New-ADOrganizationalUnit -Name "Tier0-Security-Groups" -Path $tier0OU
+New-ADOrganizationalUnit -Name "Tier0-Service-Accounts" -Path $tier0OU
 
 # Create Tier1-Management Structure
-New-ADOrganizationalUnit -Name "Tier1-Admin-Accounts" -Path $tierOneOU
-New-ADOrganizationalUnit -Name "Tier1-Admin-Workstations" -Path $tierOneOU
-New-ADOrganizationalUnit -Name "Tier1-Security-Groups" -Path $tierOneOU
-New-ADOrganizationalUnit -Name "Tier1-Servers" -Path $tierOneOU
-New-ADOrganizationalUnit -Name "Tier1-Service-Accounts" -Path $tierOneOU
+New-ADOrganizationalUnit -Name "Tier1-Admin-Accounts" -Path $tier1OU
+New-ADOrganizationalUnit -Name "Tier1-Admin-Workstations" -Path $tier1OU
+New-ADOrganizationalUnit -Name "Tier1-Security-Groups" -Path $tier1OU
+New-ADOrganizationalUnit -Name "Tier1-Servers" -Path $tier1OU
+New-ADOrganizationalUnit -Name "Tier1-Service-Accounts" -Path $tier1OU
 
 # Create Tier2-Users Structure
-New-ADOrganizationalUnit -Name "AD-Standard-Users" -Path $tierTwoOU
-New-ADOrganizationalUnit -Name "Endpoints" -Path $tierTwoOU
-New-ADOrganizationalUnit -Name "Tier2-Local-Admin-Accounts" -Path $tierTwoOU
-New-ADOrganizationalUnit -Name "Tier2-Security-Groups" -Path $tierTwoOU
+New-ADOrganizationalUnit -Name "AD-Standard-Users" -Path $tier2OU
+New-ADOrganizationalUnit -Name "Endpoints" -Path $tier2OU
+New-ADOrganizationalUnit -Name "Tier2-Local-Admin-Accounts" -Path $tier2OU
+New-ADOrganizationalUnit -Name "Tier2-Security-Groups" -Path $tier2OU
+
+Set-ADComputer -Identity "CN=DC01,OU=Domain Controllers,OU=Tier0-Control,OU=Valentine,DC=domain,DC=com" -Description "Primary Domain Controller"
 
 # Create Domain Admin
 New-ADUser -Name $givenName" $surName - Tier0 Domain Admin" -AccountPassword(ConvertTo-SecureString -String $passPhrase -AsPlainText -Force) -DisplayName $givenName" $surName - Tier0 Domain Admin" -GivenName $givenName -Surname $surName" - Tier0 Doamin Admin" -Description "Domain Administrator" -Path "OU=Tier0-Admin-Accounts,OU=Tier0-Control,OU=Valentine,DC=domain,DC=com" -Enabled $true -SamAccountName "admin-"$fullUserName -UserPrincipalName "admin-"$fullUserName"@domain.com"
