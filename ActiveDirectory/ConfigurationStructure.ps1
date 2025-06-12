6524e2efe835225f12183e7e7c401cbcd4785fbf
GM <georgemkrs@gmail.com>
2025-06-12 22:34:43 +0300

    Update ConfigurationStructure.ps1

--- ActiveDirectory/ConfigurationStructure.ps1
+++ ActiveDirectory/ConfigurationStructure.ps1
@@ -41,8 +41,8 @@ Set-ADComputer -Identity "CN=DC01,OU=Domain Controllers,DC=domain,DC=com" -Descr
 New-ADUser -Name $givenName" $surName - Tier0 Domain Admin" -AccountPassword(ConvertTo-SecureString -String $passPhrase -AsPlainText -Force) -DisplayName $givenName" $surName - Tier0 Domain Admin" -GivenName $givenName -Surname "$surName - Tier0 Domain Admin" -Description "Domain Administrator" -Path "OU=Tier0-Admin-Accounts,OU=Tier0-Control,OU=Valentine,DC=domain,DC=com" -Enabled $true -SamAccountName "admin-$fullUserName" -UserPrincipalName "admin-$fullUserName@domain.com"
 Set-ADUser -Identity "admin-$fullUserName" -PasswordNeverExpires $true
 
-Add-ADGroupMember -Identity 'Domain Admins' -Members "admin-"$fullUserName
-Add-ADGroupMember -Identity 'Enterprise Admins' -Members "admin-"$fullUserName
+Add-ADGroupMember -Identity 'Domain Admins' -Members "admin-$fullUserName"
+Add-ADGroupMember -Identity 'Enterprise Admins' -Members "admin-$fullUserName"
 
 # Create a Standard account
 New-ADUser -Name $givenName" "$surName -AccountPassword(ConvertTo-SecureString -String "password" -AsPlainText -Force) -DisplayName $givenName" "$surName -GivenName $givenName -Surname $surName -Description "General User Account" -Path "OU=AD-Standard-Userss,OU=Tier2-Users,OU=Hierarchy,DC=ADLAB,DC=HOME" -Enabled $true -SamAccountName $fullUserName -UserPrincipalName "$fullUserName@ADLAB.HOME"
