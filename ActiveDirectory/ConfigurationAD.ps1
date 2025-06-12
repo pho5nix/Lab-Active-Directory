f98fa5fd87c29bfab16b5f3a5a745efe49847091
GM <georgemkrs@gmail.com>
2025-06-12 20:43:15 +0300

    Update ConfigurationAD.ps1

--- ActiveDirectory/ConfigurationAD.ps1
+++ ActiveDirectory/ConfigurationAD.ps1
@@ -3,7 +3,7 @@ $myNetBiosName = "domain"
 $myStaticIP = "172.23.150.50"
 $myGateway = "172.23.150.254"
 $myDNS = ("172.23.150.50","172.23.150.254")
-$myExternalDNS = "1.1.1.1, 208.67.222.222"
+$myExternalDNS = "1.1.1.1","208.67.222.222"
 
 # Find the interface to set the IP address
 $adapter = Get-NetAdapter | Where-Object { $_.Status -eq "Up" } | Select-Object -First 1
@@ -12,12 +12,6 @@ $interfaceIndex = $adapter.ifIndex
 # Set IP address, Set primary and secondary DNS
 New-NetIPAddress -InterfaceIndex $interfaceIndex -AddressFamily IPv4 -IPAddress $myStaticIP -PrefixLength 24 -DefaultGateway $myGateway
 Set-DnsClientServerAddress -InterfaceIndex $interfaceIndex -ServerAddresses $myDNS
-
-# Configure NTP server
-w32tm /config /manualpeerlist:"time.windows.com" /syncfromflags:MANUAL
-net stop w32time
-net start w32time
-w32tm /resync
 
 # Install AD DS and DNS services, Install management tools
 Install-WindowsFeature AD-Domain-Services,DNS -IncludeManagementTools
@@ -28,3 +22,9 @@ Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Paramet
 
 # Add DNS forwarders for Cloudflare and OpenDNS
 Add-DnsServerForwarder -IPAddress $myExternalDNS
+
+# Configure NTP server
+w32tm /config /manualpeerlist:"time.windows.com" /syncfromflags:MANUAL
+net stop w32time
+net start w32time
+w32tm /resync
